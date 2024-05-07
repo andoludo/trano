0a5ac4d240eb26e4181a674dc04a7e6086e8eb3d
aan <andoludovic.andriamamonjy@gmail.com>
2024-05-07 22:41:40 +0200

    fix: Simple refactor.

--- tests/test_template.py
+++ tests/test_template.py
@@ -8,7 +8,7 @@ from neosim.models.elements.space import Space
 from neosim.models.elements.system import AirHandlingUnit
 from neosim.topology import Network
 
-OVERWRITE_MODELS = True
+OVERWRITE_MODELS = False
 
 
 def remove_annotation(model: str) -> str:
