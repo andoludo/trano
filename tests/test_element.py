d2903dea51e88e2f23cce4d84a926782e84faa97
aan <andoludovic.andriamamonjy@gmail.com>
2024-08-28 21:44:58 +0200

    fix: linting /cleaning

--- tests/test_element.py
+++ tests/test_element.py
@@ -5,13 +5,16 @@ from linkml.validator import validate_file
 
 def test_validate_schema() -> None:
     for name in ["boiler", "valve", "window"]:
-        boiler = Path(
-            f"/home/aan/Documents/trano/trano/models/elements/models/{name}.yaml"
+
+        element = (
+            Path(__file__)
+            .parents[1]
+            .joinpath("trano", "models", "elements", "models", f"{name}.yaml")
         )
         data_model_path = (
             Path(__file__)
             .parents[1]
             .joinpath("trano", "models", "elements", "element.yaml")
         )
-        report = validate_file(boiler, data_model_path, "Building")
+        report = validate_file(element, data_model_path, "Building")
         assert report.results == []
