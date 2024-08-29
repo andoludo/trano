40d2207624b5de3c88dd818bf29babb68df5aee5
aan <andoludovic.andriamamonjy@gmail.com>
2024-08-29 05:11:38 +0200

    fix: remove legacy code part 2.

--- trano/models/elements/envelope/floor_on_ground.py
+++ trano/models/elements/envelope/floor_on_ground.py
@@ -1,25 +1,5 @@
-from typing import Callable, List
-
-from pydantic import Field
-
 from trano.models.constants import Azimuth, Tilt
-from trano.models.elements.base import LibraryData, Port
 from trano.models.elements.envelope.base import BaseFloorOnGround
-from trano.models.elements.space import Space
-
-
-class IdeasFloorOnGround(LibraryData):
-    template: str = """
-    IDEAS.Buildings.Components.SlabOnGround {{ element.name }}(
-    redeclare parameter {{ package_name }}.Data.Constructions.
-    {{ element.construction.name }} constructionType,
-    redeclare package Medium = Medium,
-    A={{  element.surface}})"""
-    ports_factory: Callable[[], List[Port]] = Field(
-        default=lambda: [
-            Port(targets=[Space], names=["propsBus_a"], multi_connection=False),
-        ]
-    )
 
 
 class FloorOnGround(BaseFloorOnGround):
